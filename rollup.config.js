6265cd6552a546a8c8e76fd5d3720b5f639351eb
Roman Vanesyan <roman@refactr.it>
2021-03-09 22:08:40 +0500

    build: minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts'];
 const external = Object.keys(pkg.dependencies);
 
-const config = {
+export default {
 	input: 'src/index.ts',
 	external,
-
-	plugins: [
-		resolve({ extensions }),
-		babel({
-			extensions,
-			exclude: 'node_modules/**',
-			babelHelpers: 'bundled'
-		}),
-		commonjs()
-	]
-};
-
-export default {
-	...config,
 	output: [{
 		file: pkg.module,
 		format: 'esm',
@@ -31,5 +18,15 @@ export default {
 		file: pkg.main,
 		format: 'cjs',
 		sourcemap: true
-	}]
+	}],
+	plugins: [
+		resolve({ extensions }),
+		babel({
+			extensions,
+			exclude: 'node_modules/**',
+			babelHelpers: 'bundled'
+		}),
+		commonjs(),
+		terser()
+	],
 };
